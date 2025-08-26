680d3b354ce99f918ad40d7b22c7724fcb5bd998
Adzy11 <abroad32197679@gmail.com>
2025-08-26 14:30:49 +0530

    fixes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,24 +1,25 @@
-import express from "express";
-import cors from "cors";
-import "dotenv/config";
-import job from "./lib/cron.js";
-
-import authRoutes from "./routes/authRoutes.js";
-import bookRoutes from "./routes/bookRoutes.js";
-
-import { connectDB } from "./lib/db.js";
-
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+dotenv.config();
+import connectDB from './lib/db.js';
+import router from './routes/authRoutes.js';
+import booksRouter from './routes/bookRoutes.js';
+import job from "./lib/cron.js"
 const app = express();
+
+connectDB();
 const PORT = process.env.PORT || 3000;
 
-job.start();
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/auth", authRoutes);
-app.use("/api/books", bookRoutes);
+app.use('/api/auth', router);
+app.use('/api/auth', booksRouter);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-  connectDB();
-});
+job.start();
+
+
+app.listen(3000, () => {
+    console.log("server on 3000");
+})

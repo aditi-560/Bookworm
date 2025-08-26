75dce31d54b8859d3ab490a334728f35b5213d66
Adzy11 <abroad32197679@gmail.com>
2025-08-26 14:25:19 +0530

    cron functionality added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,21 +1,24 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-dotenv.config();
-import connectDB from './lib/db.js';
-import router from './routes/authRoutes.js';
-import booksRouter from './routes/bookRoutes.js';
-const app = express();
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import job from "./lib/cron.js";
 
-connectDB();
+import authRoutes from "./routes/authRoutes.js";
+import bookRoutes from "./routes/bookRoutes.js";
+
+import { connectDB } from "./lib/db.js";
+
+const app = express();
 const PORT = process.env.PORT || 3000;
 
+job.start();
 app.use(express.json());
 app.use(cors());
 
-app.use('/api/auth', router);
-app.use('/api/auth', booksRouter);
+app.use("/api/auth", authRoutes);
+app.use("/api/books", bookRoutes);
 
-app.listen(3000, () => {
-    console.log("server on 3000");
-})
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  connectDB();
+});
